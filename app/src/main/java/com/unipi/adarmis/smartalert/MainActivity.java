f186f3ca1f43a686fe27854ab97ad569251725ba
Antwnhs Darmhs <darmisantonis@gmail.com>
2023-01-24 17:04:41 +0200

    login page

--- app/src/main/java/com/unipi/adarmis/smartalert/MainActivity.java
+++ app/src/main/java/com/unipi/adarmis/smartalert/MainActivity.java
@@ -2,13 +2,47 @@ package com.unipi.adarmis.smartalert;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(this);
     }
+    @Override
+    public void onClick(View view)
+    {
+        //login function
+        TextView name = findViewById(R.id.username);
+        TextView pass = findViewById(R.id.password);
+        String username = name.getText().toString();
+        String password = pass.getText().toString();
+        if(username.equals(""))
+        {
+            Toast.makeText(this,"Username is required!",Toast.LENGTH_SHORT).show();
+        }
+        else if(password.equals(""))
+        {
+            Toast.makeText(this,"Password is required!",Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Intent intent = new Intent(this,UserPage.class);
+            startActivity(intent);
+
+        }
+
+    }
+
 }
