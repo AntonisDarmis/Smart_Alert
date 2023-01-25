8045b354e3ce2d757c1b9368d6176f7d4b3074da
n-hatz <hatzisnikitas@gmail.com>
2023-01-25 17:01:03 +0200

    login and submit incident connected to db

--- app/src/main/java/com/unipi/adarmis/smartalert/MainActivity.java
+++ app/src/main/java/com/unipi/adarmis/smartalert/MainActivity.java
@@ -1,24 +1,50 @@
 package com.unipi.adarmis.smartalert;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import android.Manifest;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, LocationListener {
     Button loginButton;
+    LocationManager locationManager;
+    private RequestQueue mRequestQueue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         loginButton = findViewById(R.id.loginButton);
         loginButton.setOnClickListener(this);
+        mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+
+
+        //set location manager
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        //request permissions
+        requestGPSPermission();
     }
     @Override
     public void onClick(View view)
@@ -38,11 +64,78 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         else
         {
-            Intent intent = new Intent(this,UserPage.class);
-            startActivity(intent);
-
+            String url = "http://192.168.1.14:8080/api/users/login";
+            JsonObjectRequest loginRequest = createLoginRequest(url,username,password);
+            mRequestQueue.add(loginRequest);
         }
 
+    }
+
+    public void requestGPSPermission()
+    {
+        //request gps permission
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},123);
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+
+    }
+
+    private JsonObjectRequest createLoginRequest(String url, String username, String password) {
+        JSONObject login_data = new JSONObject();
+        try {
+            login_data.put("username",username);
+            login_data.put("password",password);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.POST,
+                url,
+                login_data,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            Log.e("Rest response:",response.toString());
+
+                            if(response.getInt("statusCode")==200) {
+                                JSONObject user = response.getJSONObject("data").getJSONObject("user");
+                                String role = user.getString("role");
+                                if (role.equals("USER")) {
+                                    Toast.makeText(getApplicationContext(), "Επιτυχής σύνδεση!", Toast.LENGTH_LONG).show();
+                                    Intent intent = new Intent(MainActivity.this,UserPage.class);
+                                    startActivity(intent);
+                                }
+                            } else {
+                                Toast.makeText(getApplicationContext(), "Λάθος στοιχεία!", Toast.LENGTH_LONG).show();
+                            }
+                        } catch (JSONException e) {
+                            Toast.makeText(getApplicationContext(), "Internal server error. Please try again.", Toast.LENGTH_LONG).show();
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), "Internal server error. Please try again.", Toast.LENGTH_LONG).show();
+                        error.printStackTrace();
+                    }
+                }
+        );
+        return jsonObjectRequest;
     }
 
 }
--- app/src/main/java/com/unipi/adarmis/smartalert/SubmitIncident.java
+++ app/src/main/java/com/unipi/adarmis/smartalert/SubmitIncident.java
@@ -1,11 +1,20 @@
 package com.unipi.adarmis.smartalert;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -15,8 +24,20 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class SubmitIncident extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
-    Button submitButton,selectImage;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.time.LocalDate;
+
+public class SubmitIncident extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener, LocationListener {
+    Button submitButton, selectImage;
     private String category;
 
     private TextView imagePath;
@@ -25,11 +46,27 @@ public class SubmitIncident extends AppCompatActivity implements View.OnClickLis
     private TextView latitude;
     private EditText comment;
 
+    LocationManager locationManager;
+    RequestQueue mRequestQueue;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+
+        mRequestQueue = Volley.newRequestQueue(getApplicationContext());
         setContentView(R.layout.activity_submit_incident);
         imagePath = findViewById(R.id.imgPath);
         submitButton = findViewById(R.id.submitIncident);
@@ -46,6 +83,7 @@ public class SubmitIncident extends AppCompatActivity implements View.OnClickLis
         spinner.setOnItemSelectedListener(this);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onClick(View view) {
         if(view.getId() == R.id.selectImage)
@@ -56,15 +94,24 @@ public class SubmitIncident extends AppCompatActivity implements View.OnClickLis
         }
         else
         {
-            String longit = longitude.getText().toString();
-            String lat = latitude.getText().toString();
-            String comm = comment.getText().toString();
-            String imageResult = imagePath.getText().toString(); // make into blob and save to db??????????
-            Intent intent = new Intent(this,UserPage.class);
-            startActivity(intent);
+            //call post incident
+            String url = "http://192.168.1.14:8080/api/incidents/";
+
+            mRequestQueue.add(postIncident(url));
+
+
+            //Intent intent = new Intent(this,UserPage.class);
+            //startActivity(intent);
 
 
         }
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        Log.v("TAG","In location changed");
+        longitude.setText(Double.toString(location.getLongitude()));
+        latitude.setText(Double.toString(location.getLatitude()));
     }
 
     @Override
@@ -88,4 +135,55 @@ public class SubmitIncident extends AppCompatActivity implements View.OnClickLis
     public void onNothingSelected(AdapterView<?> adapterView) {
         category = "Earthquake";
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    JsonObjectRequest postIncident(String url) { //IMAGE BLOB???????
+        String longit = longitude.getText().toString();
+        String lat = latitude.getText().toString();
+        String comm = comment.getText().toString();
+        String imageResult = imagePath.getText().toString(); // make into blob and save to db??????????
+
+        JSONObject incident_data = new JSONObject();
+        try {
+            incident_data.put("date", LocalDate.now());
+            incident_data.put("latitude",Double.parseDouble(lat));
+            incident_data.put("longitude",Double.parseDouble(longit));
+            incident_data.put("type",category.toUpperCase());
+            incident_data.put("comment",comm);
+            //incident_data.put(); IMAGE PATH
+        } catch(JSONException e) {
+            e.printStackTrace();
+        }
+        return new JsonObjectRequest(
+                Request.Method.POST,
+                url,
+                incident_data,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            if(response.getInt("statusCode")==201) { //http status CREATED
+                                Toast.makeText(getApplicationContext(), "Successful incident submission", Toast.LENGTH_LONG).show();
+                                finish();
+                                startActivity(new Intent(SubmitIncident.this, UserPage.class));
+                            } else {
+                                //toast error msg
+                                Toast.makeText(getApplicationContext(), "Error submitting incident, please try again!", Toast.LENGTH_LONG).show();
+                            }
+                        } catch (JSONException e) {
+                            Toast.makeText(getApplicationContext(), "Error submitting incident, please try again!", Toast.LENGTH_LONG).show();
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), "Error submitting incident, please try again!", Toast.LENGTH_LONG).show();
+                        error.printStackTrace();
+                    }
+                }
+        );
+    }
+
 }
