d58180299776a36a5727e3f4828a8a307a6a8004
Antwnhs Darmhs <darmisantonis@gmail.com>
2023-01-26 19:18:33 +0200

    changes

---
+++ app/src/main/java/com/unipi/adarmis/smartalert/Register.java
@@ -0,0 +1,86 @@
+package com.unipi.adarmis.smartalert;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+public class Register extends AppCompatActivity  {
+    Button registerButton;
+    EditText editEmail,editPassword;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        registerButton = findViewById(R.id.registerButton);
+        editEmail = findViewById(R.id.email);
+        editPassword = findViewById(R.id.regPassword);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = String.valueOf(editEmail.getText());
+                String password = String.valueOf(editPassword.getText());
+                if(TextUtils.isEmpty(email))
+                {
+                    Toast.makeText(Register.this,"Email is required!",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(password) || email.length() <= 6 )
+                {
+                    Toast.makeText(Register.this,"Password is required and must be at least 6 characters long!",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener( new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+
+                                    Toast.makeText(Register.this, "Account created successfully",
+                                            Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(Register.this,MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(Register.this, "Account creation failed.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+            }
+        });
+    }
+}
