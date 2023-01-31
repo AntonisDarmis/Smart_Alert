c098838400948afc079970ec26584e9190488fba
n-hatz <hatzisnikitas@gmail.com>
2023-01-31 17:49:33 +0200

    background loc to toast

--- app/src/main/java/com/unipi/adarmis/smartalert/UserPage.java
+++ app/src/main/java/com/unipi/adarmis/smartalert/UserPage.java
@@ -2,16 +2,20 @@ package com.unipi.adarmis.smartalert;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Looper;
@@ -33,28 +37,39 @@ public class UserPage extends AppCompatActivity implements View.OnClickListener,
     Button submitButton, signOutButton;
     LocationManager locationManager;
     Thread triggerService;
-    Double longitude, latitude;
+    Double longitude,latitude;
 
     private FirebaseAuth mAuth;
     private FirebaseFirestore db;
 
+    private static int MY_FINE_LOCATION_REQUEST = 99;
+    private static int MY_BACKGROUND_LOCATION_REQUEST = 100;
+
+    LocationService mLocationService = new LocationService();
+    Intent mServiceIntent;
+
 
     @Override
-    public void onStart() {
+    public void onStart()
+    {
         super.onStart();
-
+        //addLocationListener();
 
     }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index_page);
+
+        /*
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION) != PackageManager.PERMISSION_GRANTED)
         {
             requestGPSPermission();
-        }
+        } */
+
+        permissionsTricky();
+        starServiceFunc();
 
         mAuth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
@@ -63,25 +78,13 @@ public class UserPage extends AppCompatActivity implements View.OnClickListener,
         submitButton.setOnClickListener(this);
 
         signOutButton = findViewById(R.id.logOutButton);
-        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 600000, 0, this);
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-
         signOutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(UserPage.this,MainActivity.class);
-                startActivity(intent);
+                starServiceFunc();
+                //FirebaseAuth.getInstance().signOut();
+                //Intent intent = new Intent(UserPage.this,MainActivity.class);
+                //startActivity(intent);
             }
         });
     }
@@ -129,6 +132,108 @@ public class UserPage extends AppCompatActivity implements View.OnClickListener,
                 });
     }
 
+    public void permissionsTricky() {
+        if (ActivityCompat.checkSelfPermission(UserPage.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+
+                if (ActivityCompat.checkSelfPermission(UserPage.this, Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+
+                    AlertDialog alertDialog = new AlertDialog.Builder(UserPage.this).create();
+                    alertDialog.setTitle("Background permission");
+                    alertDialog.setMessage("Permission dialog");
+
+                    alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Start service anyway",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    starServiceFunc();
+                                    dialog.dismiss();
+                                }
+                            });
+
+                    alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Grant background Permission",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    requestBackgroundLocationPermission();
+                                    dialog.dismiss();
+                                }
+                            });
+
+                    alertDialog.show();
+
+
+                }else if (ActivityCompat.checkSelfPermission(UserPage.this, Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED){
+                    starServiceFunc();
+                }
+            }else{
+                starServiceFunc();
+            }
+
+        }else if (ActivityCompat.checkSelfPermission(UserPage.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(UserPage.this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+
+                AlertDialog alertDialog = new AlertDialog.Builder(UserPage.this).create();
+                alertDialog.setTitle("ACCESS_FINE_LOCATION");
+                alertDialog.setMessage("Location permission required");
+
+                alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Ok",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                requestFineLocationPermission();
+                                dialog.dismiss();
+                            }
+                        });
+
+
+                alertDialog.show();
+
+            } else {
+                requestFineLocationPermission();
+            }
+        }
+
+    }
+
+    private void starServiceFunc(){
+        mLocationService = new LocationService();
+        mServiceIntent = new Intent(this, mLocationService.getClass());
+        if (!Util.isMyServiceRunning(mLocationService.getClass(), this)) {
+            startService(mServiceIntent);
+            Toast.makeText(this, "Started service", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Service already running", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private void requestBackgroundLocationPermission() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_BACKGROUND_LOCATION},
+                MY_BACKGROUND_LOCATION_REQUEST);
+    }
+
+    private void requestFineLocationPermission() {
+        ActivityCompat.requestPermissions(this,  new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, MY_FINE_LOCATION_REQUEST);
+    }
+
+    private void stopServiceFunc(){
+        mLocationService = new LocationService();
+        mServiceIntent = new Intent(this, mLocationService.getClass());
+        if (Util.isMyServiceRunning(mLocationService.getClass(), this)) {
+            stopService(mServiceIntent);
+            Toast.makeText(this, "Service stopped!!", Toast.LENGTH_SHORT).show();
+            //saveLocation(); // explore it by your self
+        } else {
+            Toast.makeText(this, "Service is already stopped!!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
     public void requestGPSPermission() {
         //request gps permission
 
@@ -140,15 +245,55 @@ public class UserPage extends AppCompatActivity implements View.OnClickListener,
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_BACKGROUND_LOCATION}, 123);
             return;
         }
-
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        Toast.makeText(this, Integer.toString(requestCode), Toast.LENGTH_LONG).show();
+
+        if ( requestCode == MY_FINE_LOCATION_REQUEST){
+
+            if (grantResults.length !=0 /*grantResults.isNotEmpty()*/ && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED) {
+                    requestBackgroundLocationPermission();
+                }
+
+            } else {
+                Toast.makeText(this, "ACCESS_FINE_LOCATION permission denied", Toast.LENGTH_LONG).show();
+                if (!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                 /*   startActivity(
+                            Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                            Uri.fromParts("package", this.getPackageName(), null),),);*/
+
+                    startActivity(new Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                            Uri.parse("package:com.unipi.adarmis.smartalert")
+                    ));
+
+                }
+            }
+            return;
+
+        }else if (requestCode == MY_BACKGROUND_LOCATION_REQUEST){
+
+            if (grantResults.length!=0 /*grantResults.isNotEmpty()*/ && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Background location Permission Granted", Toast.LENGTH_LONG).show();
+                }
+            } else {
+                Toast.makeText(this, "Background location permission denied", Toast.LENGTH_LONG).show();
+            }
+            return;
+        }
+
     }
 
-   /* private void addLocationListener() {
+    /*
+    private void addLocationListener() {
         triggerService = new Thread(new Runnable() {
             @RequiresApi(api = Build.VERSION_CODES.M)
             public void run() {
@@ -184,6 +329,5 @@ public class UserPage extends AppCompatActivity implements View.OnClickListener,
             }
         }, "LocationThread");
         triggerService.start();
-    }*/
-
+    } */
 }
